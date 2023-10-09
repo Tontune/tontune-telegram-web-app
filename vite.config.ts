b497774e0eef9be8e653f3e0afa00e3bb4f83fe7
Kirill Bondarenko <billionmann@gmail.com>
2023-10-09 10:37:57 +0300

    Handle Single Page Apps for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import basicSsl from '@vitejs/plugin-basic-ssl';
 import react from '@vitejs/plugin-react';
-import * as path from 'path';
 import { defineConfig } from 'vite';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import tsconfigPaths from 'vite-tsconfig-paths';
