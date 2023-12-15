6a7fa69803dfbbcb9c11c44470c99f9bcd3c2964
llamiib0t <max774433@gmail.com>
2023-12-15 21:54:00 +1000

    fix shadcn/ui issues

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "bg-button text-button-foreground hover:bg-button/90",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
@@ -17,7 +17,7 @@ const buttonVariants = cva(
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
+        link: "text-primary-background underline-offset-4 hover:underline",
       },
       size: {
         default: "h-10 px-4 py-2",
--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -100,7 +100,7 @@ const DialogDescription = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <DialogPrimitive.Description
     ref={ref}
-    className={cn("text-sm text-muted-foreground", className)}
+    className={cn("text-sm text-primary-foreground", className)}
     {...props}
   />
 ))
