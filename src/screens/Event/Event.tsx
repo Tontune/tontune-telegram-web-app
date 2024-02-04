f74e4ccbb2475d0db11253aa2991101e1307a4aa
llamiib0t <max774433@gmail.com>
2024-02-04 14:40:58 +1000

    resolve lint errors

--- src/screens/Event/Event.tsx
+++ src/screens/Event/Event.tsx
@@ -1,10 +1,12 @@
 import { Link, useParams } from 'react-router-dom';
-import { events } from '@/mock/events.ts';
+
 import arrowLeft from '@/assets/icons/arrow-left.svg';
-import pin from '@/assets/icons/pin.svg';
-import diamond from '@/assets/icons/diamond.svg';
-import clock from '@/assets/icons/clock.svg';
 import buttonRight from '@/assets/icons/button-right.svg';
+import clock from '@/assets/icons/clock.svg';
+import diamond from '@/assets/icons/diamond.svg';
+import pin from '@/assets/icons/pin.svg';
+import { events } from '@/mock/events.ts';
+
 import avatar4 from '../../../public/img/avatars/avatar4.png';
 
 export const Event = () => {
@@ -15,14 +17,14 @@ export const Event = () => {
 
     const eventData = events[eventId];
 
-    const { name, location, time, coverSrc } = eventData;
+    const { name, coverSrc } = eventData;
 
     return (
       <div className='w-full relative text-white'>
         <Link to={'/events'}>
           <button
             className=' m-2 absolute z-10 w-[36px] h-[36px] rounded-[50%] bg-[#0098eb] flex items-center justify-center'>
-            <img className='w-[20px] h-[20px]' src={arrowLeft} /></button>
+            <img alt={'<-'} className='w-[20px] h-[20px]' src={arrowLeft} /></button>
         </Link>
         <img src={coverSrc} className='relative rounded-3xl' alt={`${name} cover`} />
         <div className='flex justify-between items-center pt-4'>
@@ -35,7 +37,7 @@ export const Event = () => {
               <div className='text-gray-400 text-[12px]'>Artist</div>
             </div>
           </div>
-          <div><img src={buttonRight} className='w-[24px] h-[24px] text-black' /></div>
+          <div><img alt='>' src={buttonRight} className='w-[24px] h-[24px] text-black' /></div>
         </div>
 
         <div className='text-slate-950 pt-4 text-[24px]'>{name}</div>
@@ -54,11 +56,11 @@ export const Event = () => {
         <div className='pt-4'>
           <div className='text-slate-800 text-[16px]'>Data & Time</div>
           <div className='flex gap-0.5 items-center'>
-            <img src={pin} className='w-[14px] h-[14px] ' />
+            <img alt="pin" src={pin} className='w-[14px] h-[14px] ' />
             <div className='text-slate-950 text-[12px]'>25 August 2024</div>
           </div>
           <div className='flex gap-0.5 items-center'>
-            <img src={clock} className='w-[14px] h-[14px]' />
+            <img alt="time" src={clock} className='w-[14px] h-[14px]' />
             <div className='text-slate-950 text-[12px]'>19:00 PM</div>
           </div>
         </div>
@@ -140,7 +142,7 @@ export const Event = () => {
           </div>
 
           <button className='bg-sky-500 rounded-[40px] w-[100%] h-[40px] gap-2 flex justify-center items-center mt-4 text-[14px]'>
-            <img src={diamond} />
+            <img alt="buy" src={diamond} />
             <div>Buy Ticket</div>
           </button>
         </div>
