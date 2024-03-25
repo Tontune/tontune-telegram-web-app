ec9f6f4375238979c2591570fba541c7189fac13
Kirill Bondarenko <billionmann@gmail.com>
2024-03-25 19:18:09 +1000

    fix: baseUrl updated

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 /// <reference types="vite-plugin-svgr/client" />
 export default defineConfig({
   plugins: [svgr(), react(), basicSsl(), nodePolyfills(), tsconfigPaths()],
-  base: '/tontune-telegram-web-app/',
+  base: 'https://tontune.github.io/tontune-telegram-web-app/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
